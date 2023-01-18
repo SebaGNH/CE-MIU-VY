b67d5e3d53114ff84bd6fdc5a29f03ba92863910
Sebastian Gonzalez <seba.gnh@gmail.com>
2023-01-18 16:24:39 -0300

    Se agregan botones e íconos en botones

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
+# CodeEvolution Mateial UI usando Vite y Yarn
 
 
+<!--  
 
-## https://www.youtube.com/watch?v=BHEPVdfBAqE&list=PLC3y8-rFHvwh-K9mDlrrcDywl7CeVL2rO
+https://www.youtube.com/watch?v=BHEPVdfBAqE&list=PLC3y8-rFHvwh-K9mDlrrcDywl7CeVL2rO 
 
 - conocimientos
 
 - instalamos MUI
 yarn add @mui/material @emotion/react @emotion/styled
 
+
+- Instalamos iconos
+yarn add @mui/icons-material
+
+-->
 
 
 
--- src/components/M02_Button.jsx
+++ src/components/M02_Button.jsx
@@ -1,19 +1,87 @@
-import { Button } from '@mui/material'
+import { Button, Stack, IconButton } from '@mui/material'
 import React, {Fragment} from 'react'
+import { Send as SendIcon } from '@mui/icons-material';
 
 export const M02_Button = () => {
   return (
     <Fragment>
-      {/* Llamar poco la atención, pie de página o ventana emergente */}
-      <Button variant="text">Text</Button>
-      
-      {/* contained se usa para llamar la atención Ej: Registro o login*/}
-      <Button variant="contained">Contained</Button>
+      {/* Espacio de 2 y dirección flex row*/}
+      <Stack spacing={2} direction="row"> 
 
-      {/* Acciones secundarias Ej Cancelar o retroceder*/}
-      <Button variant="outline">outline</Button>
+        {/* Llamar poco la atención, pie de página o ventana emergente */}
+        <Button variant="text">Text</Button>
+
+        {/* contained se usa para llamar la atención Ej: Registro o login*/}
+        <Button variant="contained">Contained</Button>
+
+        {/* Acciones secundarias Ej Cancelar o retroceder*/}
+        <Button variant="outline">outline</Button>
+      </Stack>
 
 
+
+      {/* Segunda fila */}
+      <Stack spacing={2} direction="row">
+        {/* ir a google */}
+        <Button variant='text' href='https://www.google.com/'>ir Google</Button>
+      </Stack>
+
+
+
+      {/* Tercera fila */}
+      {/*Paleta de colores https://mui.com/material-ui/customization/palette/*/}
+      {/* palette > "los colores: primary, secondary, etc" */}
+      {/* https://mui.com/material-ui/customization/default-theme/ */}
+      <Stack spacing={2} direction="row">
+        {/* ir a google */}
+        <Button variant='contained' color='primary'>Primary</Button>
+        <Button variant='contained' color='secondary'>secondary</Button>
+        <Button variant='contained' color='error'>error</Button>
+        <Button variant='contained' color='warning'>warning</Button>
+        <Button variant='contained' color='info'>info</Button>
+        <Button variant='contained' color='success'>success</Button>
+      </Stack>
+
+
+
+    {/* Cuarta fila - Tamaños*/}
+    {/* ponemos block ya que por defecto es flex y afecta al tamaño */}
+      <Stack display="block" spacing={2} direction="row">
+        <Button variant='contained' size='small'>small</Button>
+        <Button variant='contained' size='medium'>medium</Button>
+        <Button variant='contained' size='large'>large</Button>
+      </Stack>
+
+
+
+
+      {/* Quinta fila - Iconos */}
+      <Stack spacing={2} direction="row">
+        <Button variant='contained' startIcon={<SendIcon/>}> StartIcon </Button>
+        <Button variant='contained' endIcon={<SendIcon/>}> EndIcon </Button>
+
+        {/* disableElevation = quita sombra del botón */}
+        <Button variant='contained' startIcon={<SendIcon/>} disableElevation>     
+          StartIcon 
+        </Button>
+
+        {/* Quita el efecto agua al pulsar el botón */}
+        <Button variant='contained' endIcon={<SendIcon/>} 
+          disableRipple
+        >
+          EndIcon 
+        </Button>
+      </Stack>
+
+
+
+      {/* Sexta fila - IconButton */}
+      {/* aria-label se agrega por accesibilidad */}
+      <Stack spacing={2} direction="row">
+        <IconButton aria-label='send'>
+          <SendIcon color="success" /> 
+        </IconButton>
+      </Stack>
     </Fragment>
   )
 }
